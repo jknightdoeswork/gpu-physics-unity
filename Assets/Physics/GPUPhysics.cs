344ab86906b793dc10af75f2a4cb2ebaa5c4d0be
J Knight <J Knight>
2017-04-07 22:47:40 -0600

    initial commit

---
+++ Assets/Physics/GPUPhysics.cs
@@ -0,0 +1,353 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class GPUPhysics : MonoBehaviour {
+
+	// set from editor
+	public ComputeShader m_computeShader;
+	public Mesh cubeMesh;
+	public Material cubeMaterial;
+	public Bounds m_bounds;
+
+	public float scale;
+	public float springCoefficient;
+	public float dampingCoefficient;
+	public float tangentialCoefficient;
+
+	public int x = 10;
+	public int y = 20;
+	public int z = 5;
+	public int debug_particle_id_count;
+	public int gridX;
+	public int gridY;
+	public int gridZ;
+	public Vector3 m_firstCubeLocation; // eg
+	public Vector3 m_firstCubeVelocity;
+	// calculated
+	private int total;
+	private Vector3 m_cubeScale;
+	private Matrix4x4[] m_matrices;
+
+	// data
+	private ComputeBuffer m_rigidBodyPositions;					// float3
+	private ComputeBuffer m_rigidBodyQuaternions;				// float4
+	private ComputeBuffer m_rigidBodyAngularVelocities;			// float3
+	private ComputeBuffer m_rigidBodyVelocities;				// float3
+
+	private ComputeBuffer m_particleInitialRelativePositions; 	// float3
+	private ComputeBuffer m_particlePositions;					// float3
+	private ComputeBuffer m_particleRelativePositions;			// float3
+	private ComputeBuffer m_particleVelocities;					// float3
+	private ComputeBuffer m_particleForces;						// float3
+
+
+	private ComputeBuffer m_debugParticleIds;					// int
+	private ComputeBuffer m_debugParticleVoxelPositions;		// int3 // the per particle grid locations
+	private ComputeBuffer m_voxelCollisionGrid;					// int4
+
+	private CommandBuffer m_commandBuffer;
+
+	public Vector3[] 		positionArray;							// cpu->matrix
+	public Quaternion[] 	quaternionArray;						// cpu->matrix
+	public Vector3[]		particleForcesArray;
+	public Vector3[]		particleVelocities;
+	public Vector3[]		rigidBodyVelocitiesArray;
+	public Vector3[]		particlePositions;
+	public Vector3[]		particleRelativePositions;
+	public Vector3[] 		particleInitialRelativePositions;
+
+	public int[]			voxelGridArray;
+	public int[]			particleVoxelPositionsArray;
+	public int[]			debugParticleIds;
+	private int m_kernel_generateParticleValues;
+	private int m_kernel_clearGrid;
+	private int m_kernel_populateGrid;
+	private int m_kernel_collisionDetection;
+	private int m_kernel_computeMomenta;
+	private int m_kernel_computePositionAndRotation;
+
+
+	private int m_threadGroupsPerRigidBody;
+	private int m_threadGroupsPerParticle;
+	private int m_threadGroupsPerGridCell;
+
+
+	public Vector3 gridStartPosition;
+
+	private ComputeBuffer m_bufferWithArgs;
+
+	void Start () {
+	Application.targetFrameRate = 300;
+		// Create initial positions
+		total 			= x*y*z;
+		m_matrices 		= new Matrix4x4[total];
+		positionArray 	= new Vector3[total];
+		quaternionArray	= new Quaternion[total];
+		rigidBodyVelocitiesArray = new Vector3[total];
+		m_cubeScale 	= new Vector3(scale,scale,scale);
+
+		const int particlesPerBody			= 8;
+		int n_particles						= particlesPerBody * total;
+		int numGridCells					= gridX*gridY*gridZ;
+
+		particleForcesArray = new Vector3[n_particles];
+
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				for (int k = 0; k < z; k++) {
+					positionArray[IDX(i,j,k)] = new Vector3(i*scale, j*scale, k*scale);
+					quaternionArray[IDX(i,j,k)] = Quaternion.identity;
+					rigidBodyVelocitiesArray[IDX(i,j,k)] = Vector3.zero;
+				}
+			}
+		}
+		// rigid body velocities
+		positionArray[IDX(0,0,0)] = m_firstCubeLocation;
+		rigidBodyVelocitiesArray[0] = m_firstCubeVelocity;
+
+		particleVelocities = new Vector3[n_particles];
+		particlePositions = new Vector3[n_particles];
+		particleRelativePositions = new Vector3[n_particles];
+		debugParticleIds = new int[debug_particle_id_count];
+		voxelGridArray = new int[numGridCells * 4];
+		particleVoxelPositionsArray = new int[n_particles * 3];
+
+		// Get Kernels
+		m_kernel_generateParticleValues 		= m_computeShader.FindKernel("GenerateParticleValues");
+		m_kernel_clearGrid 						= m_computeShader.FindKernel("ClearGrid");
+		m_kernel_populateGrid					= m_computeShader.FindKernel("PopulateGrid");
+		m_kernel_collisionDetection				= m_computeShader.FindKernel("CollisionDetection");
+		m_kernel_computeMomenta					= m_computeShader.FindKernel("ComputeMomenta");
+		m_kernel_computePositionAndRotation		= m_computeShader.FindKernel("ComputePositionAndRotation");
+
+		// Count Thread Groups
+		m_threadGroupsPerRigidBody				= Mathf.CeilToInt(total / 2f);
+		m_threadGroupsPerParticle				= Mathf.CeilToInt(n_particles / 8f);
+		m_threadGroupsPerGridCell				= Mathf.CeilToInt((gridX*gridY*gridZ) / 8f);
+
+		// Create initial buffers
+		const int floatThree 				= 3 * sizeof(float);
+		const int floatFour 				= 4 * sizeof(float);
+		const int intFour 					= 4 * sizeof(int);
+		const int intThree 					= 3 * sizeof(int);
+
+		m_rigidBodyPositions				= new ComputeBuffer(total, floatThree);
+		m_rigidBodyQuaternions				= new ComputeBuffer(total, floatFour);
+		m_rigidBodyAngularVelocities 		= new ComputeBuffer(total, floatThree);
+		m_rigidBodyVelocities 				= new ComputeBuffer(total, floatThree);
+
+		m_particleInitialRelativePositions	= new ComputeBuffer(n_particles, floatThree);
+		m_particlePositions					= new ComputeBuffer(n_particles, floatThree);
+		m_particleRelativePositions			= new ComputeBuffer(n_particles, floatThree);
+		m_particleVelocities				= new ComputeBuffer(n_particles, floatThree);
+		m_particleForces					= new ComputeBuffer(n_particles, floatThree);
+
+		m_voxelCollisionGrid				= new ComputeBuffer(numGridCells, intFour);
+		m_debugParticleVoxelPositions		= new ComputeBuffer(n_particles, intThree);
+		m_debugParticleIds					= new ComputeBuffer(debug_particle_id_count, sizeof(int));
+		Debug.Log("nparticles: " + n_particles);
+		// initialize constants
+		int[] gridDimensions = new int[]{gridX, gridY, gridZ};
+		m_computeShader.SetInts("gridDimensions", gridDimensions);
+		m_computeShader.SetInt("gridMax", numGridCells);
+		m_computeShader.SetFloat("particleDiameter", scale*0.5f);
+		m_computeShader.SetFloat("springCoefficient", springCoefficient);
+		m_computeShader.SetFloat("dampingCoefficient", dampingCoefficient);
+		m_computeShader.SetFloat("tangentialCoefficient", tangentialCoefficient);
+		m_computeShader.SetFloats("gridStartPosition", new float[]{gridStartPosition.x, gridStartPosition.y, gridStartPosition.z});
+		// initialize buffers
+
+
+		// initial relative positions
+		// super dependent on 8/rigid body
+		float quarterScale = scale * 0.25f;
+		particleInitialRelativePositions = new Vector3[n_particles];
+
+		Vector3[] particleInitialsSmall = new Vector3[particlesPerBody];
+		particleInitialsSmall[0] = new Vector3(quarterScale, 	quarterScale, 	quarterScale);
+		particleInitialsSmall[1] = new Vector3(-quarterScale, 	quarterScale, 	quarterScale);
+		particleInitialsSmall[2] = new Vector3(quarterScale, 	quarterScale, 	-quarterScale);
+		particleInitialsSmall[3] = new Vector3(-quarterScale, 	quarterScale, 	-quarterScale);
+		particleInitialsSmall[4] = new Vector3(quarterScale, 	-quarterScale, quarterScale);
+		particleInitialsSmall[5] = new Vector3(-quarterScale, 	-quarterScale, quarterScale);
+		particleInitialsSmall[6] = new Vector3(quarterScale, 	-quarterScale, -quarterScale);
+		particleInitialsSmall[7] = new Vector3(-quarterScale, 	-quarterScale, -quarterScale);
+
+		for (int i = 0; i < particleInitialRelativePositions.Length; i++) {
+			particleInitialRelativePositions[i] = particleInitialsSmall[i%8];
+		}
+
+		m_particleInitialRelativePositions.SetData(particleInitialRelativePositions);
+
+		// rigid body positions
+		m_rigidBodyPositions.SetData(positionArray);
+
+		// rigid body quaternions
+		m_rigidBodyQuaternions.SetData(quaternionArray);
+
+		m_rigidBodyVelocities.SetData(rigidBodyVelocitiesArray);
+
+		// Set matricies to initial positions
+		SetMatrices(positionArray, quaternionArray);
+
+		// Bind buffers
+
+		// kernel 0 GenerateParticleValues
+		m_computeShader.SetBuffer(m_kernel_generateParticleValues, "rigidBodyPositions", m_rigidBodyPositions);
+		m_computeShader.SetBuffer(m_kernel_generateParticleValues, "rigidBodyQuaternions", m_rigidBodyQuaternions);
+		m_computeShader.SetBuffer(m_kernel_generateParticleValues, "rigidBodyAngularVelocities", m_rigidBodyAngularVelocities);
+		m_computeShader.SetBuffer(m_kernel_generateParticleValues, "rigidBodyVelocities", m_rigidBodyVelocities);
+		m_computeShader.SetBuffer(m_kernel_generateParticleValues, "particleInitialRelativePositions", m_particleInitialRelativePositions);
+		m_computeShader.SetBuffer(m_kernel_generateParticleValues, "particlePositions", m_particlePositions);
+		m_computeShader.SetBuffer(m_kernel_generateParticleValues, "particleRelativePositions", m_particleRelativePositions);
+		m_computeShader.SetBuffer(m_kernel_generateParticleValues, "particleVelocities", m_particleVelocities);
+		//m_computeShader.SetBuffer(m_kernel_generateParticleValues, "debugParticleIds", m_debugParticleIds);
+
+		// kernel 1 ClearGrid
+		m_computeShader.SetBuffer(m_kernel_clearGrid, "voxelCollisionGrid", m_voxelCollisionGrid);
+
+		// kernel 2 Populate Grid
+		m_computeShader.SetBuffer(m_kernel_populateGrid, "debugParticleVoxelPositions", m_debugParticleVoxelPositions);
+		m_computeShader.SetBuffer(m_kernel_populateGrid, "voxelCollisionGrid", m_voxelCollisionGrid);
+		m_computeShader.SetBuffer(m_kernel_populateGrid, "particlePositions", m_particlePositions);
+		//m_computeShader.SetBuffer(m_kernel_populateGrid, "debugParticleIds", m_debugParticleIds);
+
+		// kernel 3 Collision Detection
+		m_computeShader.SetBuffer(m_kernel_collisionDetection, "particlePositions", m_particlePositions);
+		m_computeShader.SetBuffer(m_kernel_collisionDetection, "particleVelocities", m_particleVelocities);
+		m_computeShader.SetBuffer(m_kernel_collisionDetection, "voxelCollisionGrid", m_voxelCollisionGrid);
+		m_computeShader.SetBuffer(m_kernel_collisionDetection, "particleForces", m_particleForces);
+
+		// kernel 4 Computation of Momenta
+		m_computeShader.SetBuffer(m_kernel_computeMomenta, "particleForces", m_particleForces);
+		m_computeShader.SetBuffer(m_kernel_computeMomenta, "particleRelativePositions", m_particleRelativePositions);
+		m_computeShader.SetBuffer(m_kernel_computeMomenta, "rigidBodyAngularVelocities", m_rigidBodyAngularVelocities);
+		m_computeShader.SetBuffer(m_kernel_computeMomenta, "rigidBodyVelocities", m_rigidBodyVelocities);
+		m_computeShader.SetBuffer(m_kernel_computeMomenta, "debugParticleIds", m_debugParticleIds);
+
+		// kernel 5 Compute Position and Rotation
+		m_computeShader.SetBuffer(m_kernel_computePositionAndRotation, "rigidBodyVelocities", m_rigidBodyVelocities);
+		m_computeShader.SetBuffer(m_kernel_computePositionAndRotation, "rigidBodyAngularVelocities", m_rigidBodyAngularVelocities);
+		m_computeShader.SetBuffer(m_kernel_computePositionAndRotation, "rigidBodyPositions", m_rigidBodyPositions);
+		m_computeShader.SetBuffer(m_kernel_computePositionAndRotation, "rigidBodyQuaternions", m_rigidBodyQuaternions);
+
+
+
+		//GetComponent<InstancedIndirectRenderingTest>().InitializeRenderer(total, cubeMesh, m_rigidBodyPositions, m_rigidBodyQuaternions);
+		//m_computeShader.Dispatch(m_kernel_generateParticleValues, m_threadGroupsPerRigidBody, 1, 1);
+		//m_computeShader.Dispatch(m_kernel_clearGrid, m_threadGroupsPerGridCell, 1, 1);
+		//m_computeShader.Dispatch(m_kernel_populateGrid, m_threadGroupsPerParticle,1, 1);
+		//m_computeShader.Dispatch(m_kernel_collisionDetection, m_threadGroupsPerParticle, 1, 1);
+		//m_computeShader.Dispatch(m_kernel_computeMomenta, m_threadGroupsPerRigidBody, 1, 1);
+		//m_computeShader.Dispatch(m_kernel_computePositionAndRotation, m_threadGroupsPerRigidBody, 1, 1);
+
+
+		// Setup Indirect Renderer
+		uint indexCountPerInstance 		= cubeMesh.GetIndexCount(0);
+		uint instanceCount 				= (uint)total;
+		uint startIndexLocation 		= 0;
+		uint baseVertexLocation 		= 0;
+		uint startInstanceLocation 		= 0;
+		uint[] args = new uint[]{indexCountPerInstance, instanceCount , startIndexLocation, baseVertexLocation, startInstanceLocation};
+
+		m_bufferWithArgs = new ComputeBuffer(1, args.Length*sizeof(uint), ComputeBufferType.IndirectArguments);
+		m_bufferWithArgs.SetData(args);
+
+		cubeMaterial.SetBuffer("positions", m_rigidBodyPositions);
+		cubeMaterial.SetBuffer("quaternions", m_rigidBodyQuaternions);
+
+
+		// Setup Command Buffer
+		m_commandBuffer = new CommandBuffer();
+		m_commandBuffer.BeginSample("GenerateParticleValues");
+		m_commandBuffer.DispatchCompute(m_computeShader,m_kernel_generateParticleValues, m_threadGroupsPerRigidBody, 1, 1);
+		m_commandBuffer.EndSample("GenerateParticleValues");
+
+		m_commandBuffer.BeginSample("ClearGrid");
+		m_commandBuffer.DispatchCompute(m_computeShader, m_kernel_clearGrid, m_threadGroupsPerGridCell, 1, 1);
+		m_commandBuffer.EndSample("ClearGrid");
+
+		m_commandBuffer.BeginSample("PopulateGrid");
+		m_commandBuffer.DispatchCompute(m_computeShader, m_kernel_populateGrid, m_threadGroupsPerParticle, 1, 1);
+		m_commandBuffer.EndSample("PopulateGrid");
+
+		m_commandBuffer.BeginSample("CollisionDetection");
+		m_commandBuffer.DispatchCompute(m_computeShader, m_kernel_collisionDetection, m_threadGroupsPerParticle, 1, 1);
+		m_commandBuffer.EndSample("CollisionDetection");
+
+		m_commandBuffer.BeginSample("ComputeMomenta");
+		m_commandBuffer.DispatchCompute(m_computeShader, m_kernel_computeMomenta, m_threadGroupsPerRigidBody, 1 ,1);
+		m_commandBuffer.EndSample("ComputeMomenta");
+
+		m_commandBuffer.BeginSample("ComputePositions");
+		m_commandBuffer.DispatchCompute(m_computeShader, m_kernel_computePositionAndRotation, m_threadGroupsPerRigidBody, 1, 1);
+		m_commandBuffer.EndSample("ComputePositions");
+
+		m_commandBuffer.BeginSample("DrawMeshInstancedIndirect");
+		m_commandBuffer.DrawMeshInstancedIndirect(cubeMesh, 0, cubeMaterial, 0, m_bufferWithArgs);
+		m_commandBuffer.EndSample("DrawMeshInstancedIndirect");
+
+		Camera.main.AddCommandBuffer(CameraEvent.AfterSkybox, m_commandBuffer);
+	}
+
+
+
+	private void SetMatrices(Vector3[] positions, Quaternion[] quaternions) {
+		for (int i = 0; i < positions.Length; i++) {
+			m_matrices[i] = Matrix4x4.TRS(positions[i], quaternions[i], m_cubeScale);
+		}
+	}
+
+	private int IDX(int i, int j, int k) {
+		return i + (j * x) + (k * x * y);
+	}
+
+	void Update () {
+		//Graphics.ExecuteCommandBuffer(m_commandBuffer);
+		//Graphics.DrawMeshInstancedIndirect(cubeMesh, 0, cubeMaterial, m_bounds, m_bufferWithArgs);
+		/*
+		m_computeShader.Dispatch(m_kernel_generateParticleValues, m_threadGroupsPerRigidBody, 1, 1);
+		m_computeShader.Dispatch(m_kernel_clearGrid, m_threadGroupsPerGridCell, 1, 1);
+		m_computeShader.Dispatch(m_kernel_populateGrid, m_threadGroupsPerParticle,1, 1);
+		m_computeShader.Dispatch(m_kernel_collisionDetection, m_threadGroupsPerParticle, 1, 1);
+		m_computeShader.Dispatch(m_kernel_computeMomenta, m_threadGroupsPerRigidBody, 1, 1);
+		m_computeShader.Dispatch(m_kernel_computePositionAndRotation, m_threadGroupsPerRigidBody, 1, 1);
+		*/
+//			m_particleRelativePositions.GetData(particleRelativePositions);
+//			m_particleVelocities.GetData(particleVelocities);
+//			m_rigidBodyVelocities.GetData(rigidBodyVelocitiesArray);
+//			m_particleForces.GetData(particleForcesArray);
+			
+//			m_particlePositions.GetData(particlePositions);
+//			m_voxelCollisionGrid.GetData(voxelGridArray);
+//			m_debugParticleVoxelPositions.GetData(particleVoxelPositionsArray);
+
+			//m_rigidBodyPositions.GetData(positionArray);
+			//m_rigidBodyQuaternions.GetData(quaternionArray);
+
+//			m_debugParticleIds.GetData(debugParticleIds);
+			//SetMatrices(positionArray, quaternionArray);
+
+
+		//Graphics.DrawMeshInstanced(cubeMesh, 0, cubeMaterial, m_matrices, total, null, UnityEngine.Rendering.ShadowCastingMode.On, true, 0, Camera.main);
+		//Graphics.ExecuteCommandBuffer(m_commandBuffer);
+	}
+
+	void OnDestroy() {
+		m_rigidBodyPositions.Release();
+		m_rigidBodyQuaternions.Release();
+		m_rigidBodyAngularVelocities.Release();
+		m_rigidBodyVelocities.Release();
+
+		m_particleInitialRelativePositions.Release();
+		m_particlePositions.Release();
+		m_particleRelativePositions.Release();
+		m_particleVelocities.Release();
+		m_particleForces.Release();
+
+		m_debugParticleVoxelPositions.Release();
+		m_voxelCollisionGrid.Release();
+	}
+}
---
+++ Assets/Physics/InstancedIndirectSurfaceShader.shader
@@ -0,0 +1,135 @@
+﻿Shader "Instanced/InstancedIndirectSurfaceShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		// And generate the shadow pass with instancing support
+		#pragma surface surf Standard addshadow
+//		#pragma surface surfStandard Standard fullforwardshadows addshadow
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		// Enable instancing for this shader
+		#pragma multi_compile_instancing
+		#pragma instancing_options procedural:setup
+
+		// Config maxcount. See manual page.
+		// #pragma instancing_options
+
+		fixed4 _Color;
+		half _Glossiness;
+		half _Metallic;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+
+		#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			StructuredBuffer<float3> positions;
+			StructuredBuffer<float4> quaternions;
+		#endif
+
+		void setup()
+		{
+		#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			float3 position 	= positions[unity_InstanceID];
+			float4 q 			= quaternions[unity_InstanceID];
+			float qr			= q[0];
+			float qi			= q[1];
+			float qj			= q[2];
+			float qk			= q[3];
+
+			float4x4 rotation;
+			float4x4 translation = {
+				1,0,0,position.x,
+				0,1,0,position.y,
+				0,0,1,position.z,
+				0,0,0,1
+			};
+
+			rotation[0][0]			= 1.0f - 2.0f*qj*qj - 2.0f*qk*qk;
+			rotation[0][1]			= 2.0f*(qi*qj - qk*qr);
+			rotation[0][2]			= 2.0f*(qi*qk + qj*qr);
+			rotation[0][3]			= 0.0f;
+
+			rotation[1][0]			= 2.0f*(qi*qj+qk*qr);
+			rotation[1][1]			= 1.0f - 2.0f*qi*qi - 2.0f*qk*qk;
+			rotation[1][2]			= 2.0f*(qj*qk - qi*qr);
+			rotation[1][3]			= 0.0f;
+
+			rotation[2][0]			= 2.0f*(qi*qk - qj*qr);
+			rotation[2][1]			= 2.0f*(qj*qk + qi*qr);
+			rotation[2][2]			= 1.0f - 2.0f*qi*qi - 2.0f*qj*qj;
+			rotation[2][3]			= 0.0f;
+
+			rotation[3][0]			= 0.0f;
+			rotation[3][1]			= 0.0f;
+			rotation[3][2]			= 0.0f;
+			rotation[3][3]			= 1.0f;
+			// quaternion to matrix
+			// http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/
+			// https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation#Quaternion-derived_rotation_matrix
+
+
+
+//			unity_ObjectToWorld._11_21_31_41 = float4(1, 0, 0, 0);
+//			unity_ObjectToWorld._12_22_32_42 = float4(0, 1, 0, 0);
+//			unity_ObjectToWorld._13_23_33_43 = float4(0, 0, 1, 0);
+//			unity_ObjectToWorld._14_24_34_44 = float4(position.xyz, 1);
+			//unity_ObjectToWorld = rotation;
+			unity_ObjectToWorld = mul(translation, rotation);
+
+			
+			// inverse transform matrix
+			float3x3 w2oRotation;
+			w2oRotation[0] = unity_ObjectToWorld[1].yzx * unity_ObjectToWorld[2].zxy - unity_ObjectToWorld[1].zxy * unity_ObjectToWorld[2].yzx;
+			w2oRotation[1] = unity_ObjectToWorld[0].zxy * unity_ObjectToWorld[2].yzx - unity_ObjectToWorld[0].yzx * unity_ObjectToWorld[2].zxy;
+			w2oRotation[2] = unity_ObjectToWorld[0].yzx * unity_ObjectToWorld[1].zxy - unity_ObjectToWorld[0].zxy * unity_ObjectToWorld[1].yzx;
+
+			float det = dot(unity_ObjectToWorld[0], w2oRotation[0]);
+
+			w2oRotation = transpose(w2oRotation);
+
+			w2oRotation *= rcp(det);
+
+			float3 w2oPosition = mul(w2oRotation, -unity_ObjectToWorld._14_24_34);
+
+			unity_WorldToObject._11_21_31_41 = float4(w2oRotation._11_21_31, 0.0f);
+			unity_WorldToObject._12_22_32_42 = float4(w2oRotation._12_22_32, 0.0f);
+			unity_WorldToObject._13_23_33_43 = float4(w2oRotation._13_23_33, 0.0f);
+			unity_WorldToObject._14_24_34_44 = float4(w2oPosition, 1.0f);
+		#endif
+		}
+
+
+		// Declare instanced properties inside a cbuffer.
+		// Each instanced property is an array of by default 500(D3D)/128(GL) elements. Since D3D and GL imposes a certain limitation
+		// of 64KB and 16KB respectively on the size of a cubffer, the default array size thus allows two matrix arrays in one cbuffer.
+		// Use maxcount option on #pragma instancing_options directive to specify array size other than default (divided by 4 when used
+		// for GL).
+		//UNITY_INSTANCING_CBUFFER_START(Props)
+		//	UNITY_DEFINE_INSTANCED_PROP(fixed4, _Color)	// Make _Color an instanced property (i.e. an array)
+		//UNITY_INSTANCING_CBUFFER_END
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			// Albedo comes from a texture tinted by color
+			fixed4 c = _Color;
+			o.Albedo = c.rgb;
+			// Metallic and smoothness come from slider variables
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = c.a;
+		}
+
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
